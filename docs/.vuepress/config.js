8120495a8e6fccd1dbc7a6f929e8c5f387b96320
lmhcoding <13265878974@163.com>
2019-12-01 22:08:59 +0800

    init blog

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    title: 'lmh的前端记录',
+    description: 'lmh的前端学习笔记',
+    head: [
+        ['link', 
+            { rel: 'icon', href: '/pen.png' }
+        ],  
+    ],
+    themeConfig: {
+        nav: [
+            {text: '首页', link: '/'},
+            {text: 'Vue2.x', link: '/vue/v2/reactivity/'}
+        ]
+    }
+}
---
+++ docs/README.md
@@ -0,0 +1,7 @@
+---
+home: true
+heroImage: /learning.jpeg
+heroText: lmh的前端笔记
+tagline: 一点一滴都是进步
+actionText: 马上进入 →
+actionLink: /vue/v2/reactivity/
