e2ddb789c5605682f75834a111cd59e1e36af1a9
lmhcoding <13265878974@163.com>
2020-02-05 13:54:59 +0800

    add vuepress plugin

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,10 +3,17 @@ module.exports = {
     title: 'lmh的前端记录',
     description: 'lmh的前端学习笔记',
     head: [
-        ['link', 
-            { rel: 'icon', href: '/pen.png' }
-        ],  
+        ['link', { rel: 'icon', href: '/pen.png' } ],  
+        ['link', { rel: 'manifest', href: '/manifest.json' }],
+        ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+        ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+        ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+        ['link', { rel: 'apple-touch-icon', href: `/icons/apple-touch-icon-152x152.png` }],
+        ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
+        ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
+        ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
     ],
+    theme: '@vuepress/vue',
     themeConfig: {
         repo: 'lmhcoding/blog',
         editLinks: true,
@@ -32,5 +39,12 @@ module.exports = {
             ]
         }
     },
-    plugins: ['vuepress-plugin-table-of-contents']
+    plugins: [
+        ['@vuepress/back-to-top', true],
+        ['@vuepress/medium-zoom'],
+        ['@vuepress/pwa', {
+            serviceWorker: true,
+            updatePopup: true
+        }]
+    ]
 }
