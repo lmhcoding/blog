ec0b40354c625ec5879b4b4a78431c61ea150b0c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-18 07:51:37 +0000

    Deploy lmhcoding/blog to github.com/lmhcoding/blog.git:gh-pages

--- service-worker.js
+++ service-worker.js
@@ -27,7 +27,7 @@ self.addEventListener('message', (event) => {
 self.__precacheManifest = [
   {
     "url": "404.html",
-    "revision": "81d8bccac8d1de888f4a6a4a26537f0a"
+    "revision": "57957aba8ae912fee86a134a4fca6228"
   },
   {
     "url": "assets/css/0.styles.48263c0b.css",
@@ -38,24 +38,28 @@ self.__precacheManifest = [
     "revision": "83621669651b9a3d4bf64d1a670ad856"
   },
   {
-    "url": "assets/js/10.bd5f5607.js",
-    "revision": "c9b4cbb77cd8a94d9ec8a0305b5b9619"
+    "url": "assets/js/10.a6345a3b.js",
+    "revision": "c44aa588cf41ada59a1500a44dbb816d"
   },
   {
-    "url": "assets/js/11.396bbf02.js",
-    "revision": "77f36d24d30df2249fc17838b08858fd"
+    "url": "assets/js/11.1d6bcda9.js",
+    "revision": "bc4d625bcc5484c68a57de92a68d8a67"
   },
   {
-    "url": "assets/js/12.75269b8a.js",
-    "revision": "cb5bfa97a86c9e211a73037c3ed669df"
+    "url": "assets/js/12.cad74b5c.js",
+    "revision": "228cac6d115befdd8a34e5d4f8066abe"
   },
   {
-    "url": "assets/js/13.495f7b28.js",
-    "revision": "e032fdb5ffdca49a5bb08ed0f2bb51ee"
+    "url": "assets/js/13.805acaa7.js",
+    "revision": "d1eb0547f28faed0d2e4221b5dd32580"
   },
   {
-    "url": "assets/js/14.d78bfe9c.js",
-    "revision": "57af3a4c4aae47d47f26d81ce45632fa"
+    "url": "assets/js/14.2e5c2139.js",
+    "revision": "bd24572359e69a89ce71cb14fa176f0e"
+  },
+  {
+    "url": "assets/js/15.7c6ba7f7.js",
+    "revision": "891555a224e31116a22a5688e2d4ff6c"
   },
   {
     "url": "assets/js/2.c5bb9098.js",
@@ -66,8 +70,8 @@ self.__precacheManifest = [
     "revision": "02da2182c26b07379c86a06e4a963de9"
   },
   {
-    "url": "assets/js/4.46a478a8.js",
-    "revision": "0c9e9c1f1f07e098158f3e1ef70030f1"
+    "url": "assets/js/4.b9f182d3.js",
+    "revision": "f0c608b314f45b062db7efaee302f20f"
   },
   {
     "url": "assets/js/5.822361f8.js",
@@ -86,12 +90,12 @@ self.__precacheManifest = [
     "revision": "8f91a1e4b352cdd8e4a964112b3230c9"
   },
   {
-    "url": "assets/js/9.91a99494.js",
-    "revision": "860885f3952e056eab96493aff450070"
+    "url": "assets/js/9.8af815aa.js",
+    "revision": "1fd2dcf38162397336786bd861cc4b02"
   },
   {
-    "url": "assets/js/app.e2a594f6.js",
-    "revision": "587619f5a9603c5b648d6125516a0845"
+    "url": "assets/js/app.e825d7b3.js",
+    "revision": "986dd4f7fb8733f493959bf98d5c8195"
   },
   {
     "url": "icons/android-chrome-192x192.png",
@@ -147,7 +151,7 @@ self.__precacheManifest = [
   },
   {
     "url": "index.html",
-    "revision": "e56abdad711bea432c854db35535525c"
+    "revision": "a977ef94332c2233dfc247e6fb43a2e6"
   },
   {
     "url": "learning.jpeg",
@@ -159,31 +163,35 @@ self.__precacheManifest = [
   },
   {
     "url": "vue_v2/build.html",
-    "revision": "8852ff637d593785e646b674ba68fd53"
+    "revision": "e0dacbd22bd10018cdd075b01d489517"
+  },
+  {
+    "url": "vue_v2/component_merge.html",
+    "revision": "afef68dd0b96f05b1eb1a1547889360b"
   },
   {
     "url": "vue_v2/constructor.html",
-    "revision": "05f8333a26fd3142c1326f3efc21a0f4"
+    "revision": "d01173097c608b08202d9a6ec79e364b"
   },
   {
     "url": "vue_v2/directory.html",
-    "revision": "ef32348c55876c6e709bb625a6ded909"
+    "revision": "48bb32c0e4d29954bdca711a6546dfff"
   },
   {
     "url": "vue_v2/index.html",
-    "revision": "7d942df587dfb26aaef26b0b4a016ba4"
+    "revision": "ae51ea42be5827eaba80afc4999432c8"
   },
   {
     "url": "vue_v2/mergeOptions.html",
-    "revision": "321ba194c32ddc36e668c06511ff23d2"
+    "revision": "4e7027427ae5c220efc917372e6586d1"
   },
   {
     "url": "vue_v2/new_vue.html",
-    "revision": "61f85988cfcc4d7af50d36a05df54ac5"
+    "revision": "79a717bf65a42e310e734a1a572e6218"
   },
   {
     "url": "vue_v2/strats.html",
-    "revision": "41270d6100521cb61364eecfc8e9d4b5"
+    "revision": "0ac74849b9a276814bfb20851def25af"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
